a4e0b1394e630e53ba15be6c284b76ceded72b85
jsoria-ctrl <joaquinxignacio@gmail.com>
2025-05-26 21:18:24 -0300

    Update conexion.php

--- db/conexion.php
+++ db/conexion.php
@@ -1,1 +1,13 @@
+<?php
+$host = "localhost";
+$db = "buffet";
+$user = "root";
+$pass = "";
 
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+?>
